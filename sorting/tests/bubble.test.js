0fafcfcbb3c1fbf1726fad4cf9a4f44d0cf704c9
Ian McCausland <imccausl@gmail.com>
2019-04-27 22:47:51 -0400

    Added a second test for bubble sort

--- sorting/tests/bubble.test.js
+++ sorting/tests/bubble.test.js
@@ -29,4 +29,53 @@ describe('bubble sort', () => {
 10
 ]);
   });
+
+  test('should be able to sort even with two of the same numbers', () => {
+    const nums = [
+14,
+10,
+13,
+5,
+5,
+3,
+12,
+8,
+11,
+11,
+2,
+15,
+6,
+4,
+16,
+7,
+9,
+12,
+1,
+1
+];
+
+    bubbleSort(nums);
+    expect(nums).toEqual([
+1,
+1,
+2,
+3,
+4,
+5,
+5,
+6,
+7,
+8,
+9,
+10,
+11,
+11,
+12,
+12,
+13,
+14,
+15,
+16
+]);
+  });
 });
