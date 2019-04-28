846524d0d592d5ed1f0bed83611b847685402ed8
Ian McCausland <imccausl@gmail.com>
2019-04-28 15:07:58 -0400

    Disable array-element-newline

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     'array-bracket-newline': 'error',
     'array-bracket-spacing': 'error',
     'array-callback-return': 'error',
-    'array-element-newline': 'error',
+    'array-element-newline': 'never',
     'arrow-body-style': 'error',
     'arrow-parens': 'error',
     'arrow-spacing': [
--- sorting/tests/bubble.test.js
+++ sorting/tests/bubble.test.js
@@ -2,80 +2,16 @@ const bubbleSort = require('../bubble');
 
 describe('bubble sort', () => {
   test('should sort correctly', () => {
-    const nums = [
-10,
-5,
-3,
-8,
-2,
-6,
-4,
-7,
-9,
-1
-];
+    const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
 
     bubbleSort(nums);
-    expect(nums).toEqual([
-1,
-2,
-3,
-4,
-5,
-6,
-7,
-8,
-9,
-10
-]);
+    expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   });
 
   test('should be able to sort even with two of the same numbers', () => {
-    const nums = [
-14,
-10,
-13,
-5,
-5,
-3,
-12,
-8,
-11,
-11,
-2,
-15,
-6,
-4,
-16,
-7,
-9,
-12,
-1,
-1
-];
+    const nums = [14, 10, 13, 5, 5, 3, 12, 8, 11, 11, 2, 15, 6, 4, 16, 7, 9, 12, 1, 1];
 
     bubbleSort(nums);
-    expect(nums).toEqual([
-1,
-1,
-2,
-3,
-4,
-5,
-5,
-6,
-7,
-8,
-9,
-10,
-11,
-11,
-12,
-12,
-13,
-14,
-15,
-16
-]);
+    expect(nums).toEqual([1, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10, 11, 11, 12, 12, 13, 14, 15, 16]);
   });
 });
