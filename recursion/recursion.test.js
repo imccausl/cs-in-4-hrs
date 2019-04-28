905fc0e1abed47f0a9a6bf7cf5e1ab9681823bac
Ian McCausland <imccausl@gmail.com>
2019-04-27 21:48:31 -0400

    Intial commit

---
+++ recursion/recursion.test.js
@@ -0,0 +1,20 @@
+const { factorial, fibonacci } = require('./recursion');
+
+describe('factorial', () => {
+  test('should do factorials', () => {
+    expect(factorial(1)).toBe(1);
+    expect(factorial(2)).toBe(2);
+    expect(factorial(3)).toBe(6);
+    expect(factorial(10)).toBe(3628800);
+  });
+});
+
+describe('fibonacci', () => {
+  test('should do fibonacci', () => {
+    expect(fibonacci(10)).toBe(55);
+    expect(fibonacci(2)).toBe(1);
+    expect(fibonacci(20)).toBe(6765);
+    expect(fibonacci(15)).toBe(610);
+    expect(fibonacci(25)).toBe(75025);
+  });
+});
